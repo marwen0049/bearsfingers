cc21aacb5b3d629778751fc9d91ac7d4f844163b
marwen0049 <165537668+marwen0049@users.noreply.github.com>
2024-06-07 20:08:05 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
 document.querySelectorAll('.service-card').forEach(card => {
     card.addEventListener('mouseover', () => {
+        const serviceType = card.classList[1];
         const image = card.querySelector('.service-image');
-        image.style.animation = 'vibrate 0.3s infinite';
+        switch (serviceType) {
+            case 'graphic-design':
+                image.style.animation = 'vibrate-graphic-design 0.3s infinite';
+                break;
+            case 'branding':
+                image.style.animation = 'vibrate-branding 0.3s infinite';
+                break;
+            // Ajoutez d'autres cas pour d'autres types de services si nécessaire
+            default:
+                image.style.animation = 'vibrate 0.3s infinite';
+        }
     });
     card.addEventListener('mouseout', () => {
         const image = card.querySelector('.service-image');
