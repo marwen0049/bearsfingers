34d87579654c4da140c36808829ac95406f05782
marwen0049 <165537668+marwen0049@users.noreply.github.com>
2024-06-07 23:02:11 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
-document.querySelectorAll('.service-card').forEach(card => {
-    card.addEventListener('mouseover', () => {
-        const image = card.querySelector('.service-image');
-        image.style.animation = 'vibrate 0.3s infinite';
-    });
-    card.addEventListener('mouseout', () => {
-        const image = card.querySelector('.service-image');
-        image.style.animation = 'move 2s infinite alternate';
-    });
-});
 $(document).ready(function() {
+    // Configuration du carrousel
     $(".owl-carousel").owlCarousel({
         items: 1,
         loop: true,
         autoplay: true,
-        autoplayTimeout: 4000, // Change chaque 4 secondes
+        autoplayTimeout: 4000, // Changer de slide toutes les 4 secondes
         autoplayHoverPause: true
+    });
+
+    // Animation des images de service
+    document.querySelectorAll('.service-card').forEach(card => {
+        card.addEventListener('mouseover', () => {
+            const image = card.querySelector('.service-image');
+            image.style.animation = 'vibrate 0.3s infinite';
+        });
+        card.addEventListener('mouseout', () => {
+            const image = card.querySelector('.service-image');
+            image.style.animation = 'move 2s infinite alternate';
+        });
     });
 });
