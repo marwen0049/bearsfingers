45de5fc57b49d00981d2c6507feefdd39ab56330
marwen0049 <165537668+marwen0049@users.noreply.github.com>
2024-06-07 23:00:09 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,3 +8,12 @@ document.querySelectorAll('.service-card').forEach(card => {
         image.style.animation = 'move 2s infinite alternate';
     });
 });
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000, // Change chaque 4 secondes
+        autoplayHoverPause: true
+    });
+});
