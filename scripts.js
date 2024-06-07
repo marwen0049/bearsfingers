3d348f580f943efa7f42adeff1310701467d4640
marwen0049 <165537668+marwen0049@users.noreply.github.com>
2024-06-07 20:31:27 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,39 +1,4 @@
-$(document).ready(function() {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true
-    });
-
-    // Afficher la popup lorsque vous cliquez sur une image de service
-    document.querySelectorAll('.service-image').forEach(image => {
-        image.addEventListener('click', () => {
-            const serviceType = image.parentElement.id;
-            const popup = document.querySelector(`.${serviceType}-popup`);
-            popup.style.display = 'block';
-        });
-    });
-
-    // Fermer la popup lorsque vous cliquez en dehors du contenu
-    document.querySelectorAll('.service-popup').forEach(popup => {
-        popup.addEventListener('click', (event) => {
-            if (!event.target.closest('.popup-content')) {
-                popup.style.display = 'none';
-            }
-        });
-    });
-
-    // Animation de la souris sur les cartes de service
-    document.querySelectorAll('.service-card').forEach(card => {
-        card.addEventListener('mouseover', () => {
-            const image = card.querySelector('.service-image');
-            image.style.animation = 'vibrate 0.3s infinite';
-        });
-        card.addEventListener('mouseout', () => {
-            const image = card.querySelector('.service-image');
-            image.style.animation = 'move 2s infinite alternate';
-        });
-    });
-});
+document.querySelectorAll('.service-card').forEach(card => {
+    card.addEventListener('mouseover', () => {
+        const serviceType = card.classList[1]; // Obtenez la deuxième classe qui identifie le type de service
+        const image =
