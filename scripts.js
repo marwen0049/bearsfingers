ebc9c3d79e8b1e36812a63d6251ef546e02db8e4
marwen0049 <165537668+marwen0049@users.noreply.github.com>
2024-06-29 18:45:42 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -85,14 +85,6 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburgerMenu = document.getElementById('hamburger-menu');
-    const mobileMenu = document.getElementById('mobile-menu');
-
-    hamburgerMenu.addEventListener('click', function() {
-        mobileMenu.classList.toggle('open');
-    });
-});
 <script>
 document.addEventListener("DOMContentLoaded", function() {
     const hamburgerMenu = document.getElementById('hamburger-menu');
