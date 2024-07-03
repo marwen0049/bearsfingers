6671976abb61c403163de03bb7d136ec194255b0
marwen0049 <165537668+marwen0049@users.noreply.github.com>
2024-07-03 12:30:31 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -85,3 +85,12 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+ <!-- Chargement des scripts JavaScript nécessaires -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#header-container').load('header.html');
+            $('#footer-container').load('footer.html');
+        });
+    </script>
+
